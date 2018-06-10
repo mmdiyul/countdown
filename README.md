09c7cbde29a2cd0a0e097ee1a851388baba52b19
mmdiyul <muhammadaliyulm@gmail.com>
2018-06-10 16:08:44 +0700

    Update Everything ...

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# countdown
+# Countdown
+Countdown using JavaScript ~
+## Installation
+1. Clone the repository
+```
+git clone https://github.com/mmdiyul/countdown.git
+```
+2. Go to `countdown` directory
+3. Open `index.html` on your browser
+## Configuration
+You can set your own date by change the value of `countDownDate` in the directory below
+```
+assets/js/countdown.js
+```
---
+++ assets/js/countdown.js
@@ -0,0 +1,33 @@
+// Tanggal dan waktu akhir countdown
+var countDownDate = new Date("Jun 14, 2018 18:00:00").getTime();
+
+// Update setiap 1 detik
+var countdownfunction = setInterval(function() {
+
+    // Ambil tanggal dan waktu sekarang
+    var now = new Date().getTime();
+
+    // Hitung selisih tanggal dan waktu
+    var distance = countDownDate - now;
+
+    // Proses perhitungan waktu per hari hingga detik
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output hasil ke id
+    document.getElementById("countdownDays").innerHTML = days;
+    document.getElementById("countdownHours").innerHTML = hours;
+    document.getElementById("countdownMinutes").innerHTML = minutes;
+    document.getElementById("countdownSeconds").innerHTML = seconds;
+
+    // Jika waktu countdown habis, maka
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.getElementById("countdownDays").innerHTML = 0;
+        document.getElementById("countdownHours").innerHTML = 0;
+        document.getElementById("countdownMinutes").innerHTML = 0;
+        document.getElementById("countdownSeconds").innerHTML = 0;
+    }
+}, 1000);
